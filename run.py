d96eac4faeed7b49dd27ff974711666a5d8236f0
Karthik <kraj@twilio.com>
2017-05-03 18:53:02 -0700

    Delete copy-pasta code

--- run.py
+++ run.py
@@ -36,68 +36,6 @@ logger.addHandler(ch)
 app = Flask(__name__)
 
 
-def get_birthday_list_from_file():
-    with open(os.path.join(MYDIR, 'birthdays.csv'), 'rb') as fp:
-        reader = csv.reader(fp)
-        birthday_list = list(reader)
-    return birthday_list
-
-
-def find_matching_contact_detail(birthday_list, phone_number):
-    for contact_detail in birthday_list:
-        if phone_number == contact_detail[4]:
-            return contact_detail
-    return None
-
-
-@app.route("/sms", methods=['POST'])
-def receive_message():
-    """Respond to incoming calls with a simple text message."""
-    pprint(request.values)
-    text_response = request.values.get("Body")
-    phone_number = request.values.get("From")
-    birthday_list = get_birthday_list_from_file
-
-    contact_detail = find_matching_contact_detail(birthday_list, phone_number)
-
-    first_name = phone_number
-    last_name = ""
-
-    if contact_detail is not None:
-        first_name = contact_detail[0]
-        last_name = contact_detail[1]
-
-    resp = "{} {} sent {}".format(first_name, last_name, text_response)
-    client.messages.create(to="+16144775689", from_="+14152003278", body=resp)
-    logger.info("Response received from {} {}".format(first_name, last_name))
-    # resp = twilio.twiml.Response()
-    # resp.message("Hello, Mobile Monkey")
-    # return str(resp)
-
-
-@app.route("/sms", methods=['GET'])
-def send_birthday_message():
-    """Parses csv file and sends birthday message for people whose birthday falls on the current day"""
-
-    birthday_list = get_birthday_list_from_file()
-
-    today = date.today()
-    print today
-    birthday_wishes_sent = "Today's birthday list:\n"
-    for contact_detail in birthday_list:
-        birthday = contact_detail[2]
-        date_object = datetime.strptime(birthday, "%m/%d/%Y")
-
-        # Check if the month and day matches the current month and day
-        if date_object.month == today.month and date_object.day == today.day:
-            birthday_message = contact_detail[3]
-            phone_number = contact_detail[4]
-            client.messages.create(to=phone_number, from_="+14152003278", body=birthday_message)
-            birthday_wishes_sent = birthday_wishes_sent + contact_detail[0] + " " + contact_detail[1] + "\n"
-            print "Sent birthday wishes to " + contact_detail[0]
-    return birthday_wishes_sent
-
-
 @app.route("/", methods=['GET', 'POST'])
 def greet_user():
     resp = VoiceResponse()
